2b1ede9444b04ba8b9978b86f4a69d864b5a8844
2-chanhee <l01083582454@gmail.com>
2022-10-01 16:11:25 +0800

    asdfasdfdsa

--- src/common/common.service.ts
+++ src/common/common.service.ts
@@ -3,7 +3,7 @@ import {Injectable} from '@nestjs/common';
 @Injectable()
 export class CommonService {
     getStatus(): string {
-        console.log(123);
+        console.log(1232);
         return 'healthy';
     }
 }
